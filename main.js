5d1bf45603b56c4291a16e53b9df9315b211eacb
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-11-24 20:54:38 +0530

    adding functionality to buttons

--- main.js
+++ main.js
@@ -2,22 +2,48 @@ import menu from "./data";
 
 let allDishes=document.getElementById('allDishes');
 let content="";
-menu.map((dish)=>{
-  let individualDish=`
-  <div class="dish">
-      <img src="${dish.img}" alt="">
-      <div class="dishOuter">
-        <div class="dishInner">
-          <h3 class="dishName">${dish.title}</h3>
-          <h3 class="dishPrice">${dish.price}</h3>
-        </div>
-        
-        
-        <p>${dish.desc}</p>
-      </div>
-    </div>
-  `
-  content += individualDish
-})
 
-allDishes.innerHTML=content
+displayAll(menu)
+
+let allButtons=document.querySelectorAll('button');
+allButtons.forEach((button)=>button.addEventListener('click',(e)=>{filterItem(e.target.innerText)}))
+
+function displayAll(dishes){
+  content=' ';
+  dishes.map((dish)=>{
+    let individualDish=`
+    <div class="dish">
+        <img src="${dish.img}" alt="">
+        <div class="dishOuter">
+          <div class="dishInner">
+            <h3 class="dishName">${dish.title}</h3>
+            <h3 class="dishPrice">${dish.price}</h3>
+          </div>
+          
+          
+          <p>${dish.desc}</p>
+        </div>
+      </div>
+    `
+    content += individualDish
+  })
+  
+  allDishes.innerHTML=content;
+  
+}
+
+
+// filter logic here
+
+
+function filterItem(category){
+  allDishes.innerHTML +=' ';
+  if(category == 'All'){
+    displayAll(menu)
+  }
+  else{
+    let filteredItems=menu.filter((dish)=>dish.category.toLowerCase()==category.toLowerCase())
+    displayAll(filteredItems)
+  }
+  
+}
